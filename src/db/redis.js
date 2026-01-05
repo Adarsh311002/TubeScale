e2c44ee56f4f11b1821bfee3899c6d609e39e1e1
adarsh311002 <adarsh.hit.aeie25@heritageit.edu.in>
2026-01-05 12:15:32 +0530

    feat(backend): refactor server initialization

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,14 +1,21 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-    url: process.env.REDIS_URL || 'redis://localhost:6379'
-})
+  url: process.env.REDIS_URL || "redis://redis:6379",
+});
 
 redisClient.on('error', (err) => console.log("Redis client error", err));
+redisClient.on("connect", () => console.log(" Redis Client Connected"));
 
 const connectRedis = async () => {
-    await redisClient.connect();
-    console.log("Redis connected Successfully");
-}
+    try {
+        await redisClient.connect();
+        console.log("Redis Connection Established");
+    } catch (error) {
+        console.error("Redis Connection Failed:", error);
+        process.exit(1); 
+    }
+};
+
 
 export {redisClient,connectRedis};
